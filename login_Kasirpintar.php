f4f628363509e04027afb79b352e7aa1a64322e2
bimenbos <90279782+bimenbos@users.noreply.github.com>
2022-04-06 14:45:07 +0700

    Add files via upload

--- login_Kasirpintar.php
+++ login_Kasirpintar.php
@@ -5,13 +5,15 @@
                     <p class="font-32-sb font-black" align="center">Login Owner</p>
                     <p class="font-16-r font black" align="center">Belum punya akun? <a href="https://kasirpintar.co.id/register?utm_source=web&utm_medium=login_page&utm_campaign=button1" id="btn_reg_login" class="font-green">Daftar di sini</a></p>
                                         <div class="mt-30">
+                    <center>
                         <p class="font-14-r font-gray">Masuk Sebagai</p>
                         <select id="sebagai" class="form-select mt-5-2 font-14-r" onchange="myFunction()">
                             <option class="font-14-r" value="owner" selected>Owner</option>
                             <option class="font-14-r" value="staff" >Staff</option>
                         </select>
                     </div>
-                    <form action="https://kasirpintar.co.id/login" class="mt-20" method="post">
+                    <center>
+                        <form action="https://kasirpintar.co.id/login" class="mt-20" method="post">
                         <input type="hidden" name="_token" value="M8atnKnGHYrkQ5Ex1X5A7Wo47NvBjuwHpABipbtM">
                         <input hidden name="url_cuss" value="https://kasirpintar.co.id/account">
                         <div class="form-group">
@@ -24,8 +26,10 @@
                             <span onclick="showPassword()" class="fa fa-fw fa-eye pass-icon form-eye show-password" id="showPass"></span>
                                                     </div>
                         <button type="submit" class="button-green w-100 radius-8 h-40 mt-20 font-14-m">Masuk</button>
+                    </center>
                         <p class="mt-5-2" align="right"><a href="https://kasirpintar.co.id/password/reset" class="font-14-r font-green">Lupa Password?</a></p>
                     </form>
+                </center>
                 </div>
                 <hr class="mt-30">
                 <p class="font-14-r font-gray mt-20" align="center">Atau Login dengan Google</p>
